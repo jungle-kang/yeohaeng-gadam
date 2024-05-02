b3f4bdfe029b1eca7a8dd9086a3edeaf543078e4
ljo1010 <ljo1010@gmail.com>
2024-05-02 20:50:31 +0900

    signIn 누를 시 모달창 나옴

--- yeohaeng-gadam-fe/src/components/Header.tsx
+++ yeohaeng-gadam-fe/src/components/Header.tsx
@@ -1,19 +1,29 @@
-import {useNavigate} from "react-router-dom";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Modal from './Modal';
+import '../index.css';
 
-export default function Header(){
-    const navigate = useNavigate();
-    return (
-        <header className="w-full h-20 flex flex-low bg-white mt-2">
-            <button
-                onClick={()=>navigate('/')}
-                className="basis-1/5 font-bold text-4xl p-4 logo-font">
-                여행가담
-            </button>
-            <div className="basis-3/5"></div>
-            <div className="basis-1/5 h-full">
-                <button className="w-1/2 h-full logo-font hover:text-gray-400">signIn</button>
-                <button className="w-1/2 h-full logo-font hover:text-gray-400">signUp</button>
-            </div>
-        </header>
-    )
+export default function Header() {
+  const navigate = useNavigate();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <header className="w-full h-20 flex flex-row bg-white mt-2">
+      <button
+        onClick={() => navigate('/')}
+        className="basis-1/5 font-bold text-4xl p-4 logo-font">
+        여행가담
+      </button>
+      <div className="basis-3/5"></div>
+      <div className="basis-1/5 h-full flex">
+        <button className="w-1/2 h-full logo-font hover:text-gray-400" onClick={() => setModalOpen(true)}>
+          signIn
+        </button>
+        <button className="w-1/2 h-full logo-font hover:text-gray-400">
+          signUp
+        </button>
+      </div>
+      <Modal isOpen={modalOpen} closeModal={() => setModalOpen(false)} />
+    </header>
+  );
 }

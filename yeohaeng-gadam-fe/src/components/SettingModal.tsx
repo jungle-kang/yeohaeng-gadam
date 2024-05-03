c8a88567563ef12cad38f10ca8ba11f8f2cde664
ljo1010 <ljo111004@gmail.com>
2024-05-03 21:23:58 +0900

    링크복사시 애니메이션 추가

--- yeohaeng-gadam-fe/src/components/SettingModal.tsx
+++ yeohaeng-gadam-fe/src/components/SettingModal.tsx
@@ -4,6 +4,8 @@ import GoogleLoginButton from '../login/jwtgoogle';
 import Home from "../pages/Home.tsx";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useParams } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SettingModal = ({ isOpen, closeModal }) => {
   const modalBackground = useRef();
@@ -14,18 +16,16 @@ const SettingModal = ({ isOpen, closeModal }) => {
   const { roomId } = useParams(); // URL 파라미터에서 roomId 추출
   const url = `http://localhost:5173/${roomId}`;
 
-
   const copyToClipboard = () => {
     navigator.clipboard.writeText(url)
       .then(() => {
-        alert('URL이 클립보드에 복사되었습니다!');
+        toast('URL이 클립보드에 복사되었습니다!'); // 성공 메시지를 toast로 표시
       })
       .catch(err => {
         console.error('클립보드 복사에 실패했습니다:', err);
+        toast.error('클립보드 복사에 실패했습니다!'); // 실패 메시지를 toast로 표시
       });
   };
-
-
 
   return (
     <div className={'modal-container'} ref={modalBackground} onClick={e => {
@@ -35,12 +35,11 @@ const SettingModal = ({ isOpen, closeModal }) => {
     }}>
       <div className={'modal-content rounded-lg'}>
         <div>
-            <div className=' text-center mt-3'>
-            {/* <input className='border-2' placeholder='초대링크'/> */}
-            <button onClick={copyToClipboard}>링크 복사</button>
-            </div>
+          <div className='text-center mt-3'>
+            <button onClick={copyToClipboard}>링크 복사🔗</button>
+          </div>
           <div className="text-center ">
-            <button onClick={()=>closeModal()} className=" bg-slate-300 text-center p-2 mt-10 rounded-lg">메인화면</button>
+            <button onClick={()=>closeModal()} className="bg-slate-300 text-center p-2 mt-10 rounded-lg">메인화면</button>
             <br/>
             <button className="bg-slate-300 text-center p-2 mt-10 rounded-lg">확정하기</button>
             <br/>            
@@ -48,12 +47,10 @@ const SettingModal = ({ isOpen, closeModal }) => {
           </div>         
         </div>
         <div>
-          <div className='ml-96 mb-96 top-20'>
-          
-          </div>
+          <div className='ml-96 mb-96 top-20'></div>
         </div>
-        
       </div>
+      <ToastContainer position="top-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
     </div>
   );
 };

0bafeb34dbd191a83c66f8977aed5561ef99e92b
hyeinKim <kimnieyh@gmail.com>
2024-05-03 21:58:52 +0900

    Home화면 Searchbar 스크롤시 상단 고정

--- yeohaeng-gadam-fe/src/pages/Home.tsx
+++ yeohaeng-gadam-fe/src/pages/Home.tsx
@@ -3,14 +3,16 @@ import RoomList from "../components/RoomList.tsx";
 
 const Home = () => {
     return (
-        <main className="w-full h-screen">
+        <main className="w-full h-screen top-0">
             <div>
                 <img className="object-cover h-[400px] w-full"
                      src="https://images.pexels.com/photos/2407070/pexels-photo-2407070.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                      alt="main image"/>
             </div>
-            <SearchBar/>
-            <RoomList/>
+            <div className="h-screen overflow-y-none">
+                <SearchBar/>
+                <RoomList/>
+            </div>
         </main>
     )
 }

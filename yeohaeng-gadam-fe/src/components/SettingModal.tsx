402148219a08e5ef326f2773ac9c9a5574cb1350
ljo1010 <ljo111004@gmail.com>
2024-05-03 20:53:33 +0900

    링크복사

--- yeohaeng-gadam-fe/src/components/SettingModal.tsx
+++ yeohaeng-gadam-fe/src/components/SettingModal.tsx
@@ -3,12 +3,29 @@ import '../index.css';
 import GoogleLoginButton from '../login/jwtgoogle';
 import Home from "../pages/Home.tsx";
 import { Navigate, useNavigate } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 const SettingModal = ({ isOpen, closeModal }) => {
   const modalBackground = useRef();
   const navigate = useNavigate();
 
   if (!isOpen) return null;
+
+  const { roomId } = useParams(); // URL 파라미터에서 roomId 추출
+  const url = `http://localhost:5173/${roomId}`;
+
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(url)
+      .then(() => {
+        alert('URL이 클립보드에 복사되었습니다!');
+      })
+      .catch(err => {
+        console.error('클립보드 복사에 실패했습니다:', err);
+      });
+  };
+
+
 
   return (
     <div className={'modal-container'} ref={modalBackground} onClick={e => {
@@ -18,6 +35,10 @@ const SettingModal = ({ isOpen, closeModal }) => {
     }}>
       <div className={'modal-content rounded-lg'}>
         <div>
+            <div className=' text-center mt-3'>
+            {/* <input className='border-2' placeholder='초대링크'/> */}
+            <button onClick={copyToClipboard}>링크 복사</button>
+            </div>
           <div className="text-center ">
             <button onClick={()=>closeModal()} className=" bg-slate-300 text-center p-2 mt-10 rounded-lg">메인화면</button>
             <br/>

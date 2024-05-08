ef94ec64a876176e4cae6374192a1a8013e1e70b
김수빈 <ambitiousksb@gmail.com>
2024-05-08 15:26:25 +0900

    Feat : feature/7-room [Get] entry/all [Get] entry/user [Patch] room/state DB admission Table Drop DB entry Table Create room Folder Rename to room-tag Folder

--- yeohaeng-gadam-be/src/main.ts
+++ yeohaeng-gadam-be/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupSwagger } from "./util/swagger";
+import { setupSwagger } from './util/swagger';
 import * as dotenv from 'dotenv';
 import { ConfigService } from '@nestjs/config';
 import { WebsocketAdapter } from './chat/websocket-adapter';
--- yeohaeng-gadam-be/src/room-tag/room-tag.module.ts
+++ yeohaeng-gadam-be/src/room-tag/room-tag.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { RoomService } from './room.service';
-import { RoomController } from './room.controller';
+import { RoomService } from './room-tag.service';
+import { RoomController } from './room-tag.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Admission } from './entities/admission.entity';
+import { Entry } from '../entry/entities/entry.entity';
 import { Room } from './entities/room.entity';
 import { Tag } from './entities/tag.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Room, Tag, Admission])],
+  imports: [TypeOrmModule.forFeature([Room, Tag, Entry])],
   controllers: [RoomController],
   providers: [RoomService],
 })
-export class RoomModule {}
+export class RoomTagModule {}

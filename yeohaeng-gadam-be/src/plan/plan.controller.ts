ae6206f497db39cdb03cd33b19a1e14388879786
minsky922 <minsky922@gmail.com>
2024-05-09 16:11:51 +0900

    swagger

--- yeohaeng-gadam-be/src/plan/plan.controller.ts
+++ yeohaeng-gadam-be/src/plan/plan.controller.ts
@@ -2,18 +2,27 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PlanService } from './plan.service';
 import { CreatePlanDto } from './dto/create-plan.dto';
 import { UpdatePlanDto } from './dto/update-plan.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-
+@ApiTags('plans')
 @Controller('plan')
 export class PlanController {
   constructor(private readonly planService: PlanService) { }
 
   @Post('/:roomId')
+  @ApiOperation({
+    summary: 'day 생성',
+    description: ' roomId에 해당하는 day 생성',
+  })
   async createDay(@Param('roomId') roomId: string) {
     return await this.planService.createDay(roomId);
   }
 
   @Post('/:roomId/:day')
+  @ApiOperation({
+    summary: '계획 생성',
+    description: ' roomId와 day에 해당하는 계획 생성',
+  })
   async createPlan(
     @Param('roomId') roomId: string,
     @Param('day') day: number,
@@ -22,6 +31,10 @@ export class PlanController {
   }
 
   @Get('/:roomId/:day')
+  @ApiOperation({
+    summary: '계획 조회',
+    description: ' roomId와 day에 해당하는 계획 조회',
+  })
   async getPlan(
     @Param('roomId') roomId: string,
     @Param('day') day: number,
@@ -32,6 +45,10 @@ export class PlanController {
 
 
   @Patch('/:roomId/:day')
+  @ApiOperation({
+    summary: '계획 조회',
+    description: ' roomId와 day에 해당하는 계획 생성',
+  })
   async updatePlan(
     @Param('roomId') roomId: string,
     @Param('day') day: number,

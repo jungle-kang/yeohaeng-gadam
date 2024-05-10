cdc8cd6671681e5a7a8694a29941f6aaeaac0ee2
Ubuntu <ubuntu@ip-172-31-13-116.ap-northeast-2.compute.internal>
2024-05-10 05:39:58 +0000

    KMS Room.tst rollback

--- yeohaeng-gadam-fe/src/pages/Room.tsx
+++ yeohaeng-gadam-fe/src/pages/Room.tsx
@@ -1,11 +1,13 @@
 // import {useParams} from "react-router-dom";
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, {useEffect} from "react";
+import {useNavigate, useParams} from "react-router-dom";
 import { createClient } from "@liveblocks/client";
 import { LiveMap, LiveObject } from "@liveblocks/client";
 import { RoomProvider, useMutation } from "/liveblocks.config";
 
 import RoomContent from "../components/RoomContent";
+import {getCookie} from "./TestBoard.tsx";
+import {jwtDecode} from "jwt-decode";
 
 const API_KEY = import.meta.env.VITE_LIVEBLOCKS_API_PUBLIC;
 
@@ -15,10 +17,57 @@ const client = createClient({
 });
 
 const Room = () => {
-  const {roomId} = useParams<{roomId:string}>();
-  const roomProviderId = "whiteboard-" + roomId
+    const accessToken:string = getCookie('access_token')? getCookie('access_token'):'' ;
+    let id = '';
+    // @ts-ignore
+    if(accessToken!== '') {
+        id = jwtDecode(accessToken).id;
+    }
+    const navigate = useNavigate();
+    const {roomId} = useParams<{roomId:string}>();
+    const roomProviderId = "whiteboard-" + roomId
 
-  return (
+    useEffect(() => {
+        const enter = async () => {
+            try{
+                console.log('room id:',roomId,', user id:',id);
+                const response = await fetch(`/api/room/enter?room_id=${roomId}&user_id=${id}`,{
+                    method: 'PATCH',
+                    credentials: 'include'
+                }).then(res =>res.json())
+                console.log('enter response:',response);
+                if (!response.data){
+                    alert('로그인이 필요합니다.111');
+                    navigate('/');
+                }
+            }catch(e){
+                console.error('enter error:',e);
+            }
+        }
+        const meCheck = async () =>{
+            try{
+                const response = await fetch(`/api/auth/me/${id}`,{
+                    method: 'GET',
+                    credentials: 'include',
+                }).then(res=>res.json())
+                if(response.data) {
+                    enter();
+                }else{
+                    alert('로그인이 필요합니다.222');
+                    navigate('/');
+                }
+            }catch(e){
+                console.log(e);
+            }
+        }
+        if (accessToken){
+            meCheck();
+        }else {
+            alert('로그인이 필요합니다.333');
+            navigate('/');
+        }
+    }, []);
+    return (
         <RoomProvider
           id={roomProviderId}
           initialPresence={{
@@ -36,9 +85,9 @@ const Room = () => {
             // lines: new LiveMap(),
           }}
         >
-          <RoomContent roomId={roomProviderId}/>
+          <RoomContent />
         </RoomProvider>
-  )
+        )
 }
 
 export default Room

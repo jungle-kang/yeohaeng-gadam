5c8f44fbd6ddd98f43fd475ec8651edf4e3c1a91
ljo1010 <ljo111004@gmail.com>
2024-05-11 13:46:03 +0900

    upload test

--- yeohaeng-gadam-fe/src/main.tsx
+++ yeohaeng-gadam-fe/src/main.tsx
@@ -81,7 +81,7 @@ const router = createBrowserRouter([
     {
         path: "/zoom",
         element: <Videochat />
-    },
+    }
 
 
 
@@ -94,6 +94,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             {/* <React.StrictMode> */}
             <RouterProvider router={router} />
             {/* </React.StrictMode> */}
-        </GoogleOAuthProvider>,
+        </GoogleOAuthProvider>
     </CookiesProvider>
 )

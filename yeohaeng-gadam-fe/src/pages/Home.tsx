66c202805d986b99feb908547137e665bd0d934d
hyeinKim <kimnieyh@gmail.com>
2024-05-15 19:59:30 +0900

    이미지위에 글자 변경

--- yeohaeng-gadam-fe/src/pages/Home.tsx
+++ yeohaeng-gadam-fe/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import SearchBar from "../components/SearchBar.tsx";
 import RoomList from "../components/RoomList.tsx";
 import PopularLocation from "../components/PopularLocation.tsx";
@@ -8,21 +8,21 @@ import 'react-toastify/dist/ReactToastify.css';
 const Home = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const images = [
-        "/img/main/강릉1_1920.jpg",
-        "/img/main/강릉2_1920.jpg",
-        "/img/main/강릉3_1920.jpg",
-        "/img/main/경복궁1_1920.jpg",
+        // "/img/main/강릉1_1920.jpg",
+        // "/img/main/강릉2_1920.jpg",
+        // "/img/main/강릉3_1920.jpg",
+        // "/img/main/경복궁1_1920.jpg",
         "/img/main/경복궁2_1920.jpg",
-        "/img/main/광안대교_1920.jpg",
-        "/img/main/서울 불꽃축제_1920.jpg",
         "/img/main/영월_1920.jpg",
+        "/img/main/광안대교_1920.jpg",
+        // "/img/main/서울 불꽃축제_1920.jpg",
         "/img/main/제주_1920.jpg"
     ];
 
     useEffect(() => {
         const intervalId = setInterval(() => {
             setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 3000);
+        }, 6000);
 
         return () => clearInterval(intervalId);
     }, [images.length]);
@@ -40,10 +40,10 @@ const Home = () => {
                         alt={`Image ${index}`}
                     />
                 ))}
-                <div className="absolute top-0 left-0 w-full h-[450px] flex items-center justify-center">
+                <div className="absolute top-0 left-0 w-full h-[450px] flex items-center justify-start pl-20">
                     <div>
-                        <h3 className="nanumbarungothic tracking-wide text-white text-2xl md:text-5xl font-bold">여행 계획을 짤 땐</h3>
-                        <h3 className="nanumbarungothic tracking-wide text-white text-2xl md:text-5xl font-bold text-center mt-2 ">여행가담</h3>
+                        <h3 className="tracking-tight tracking-wide text-white text-xl md:text-2xl shadow-5xl">동시계획 화이트보드</h3>
+                        <h3 className="nanumbarungothic tracking-wide text-white text-2xl md:text-4xl shadow-5xl">여행계획은 여행가담</h3>
                     </div>
 
                 </div>

13acafd28015a7ccfe0c8fd95dba4e2f8931315e
hyeinKim <kimnieyh@gmail.com>
2024-05-15 19:06:09 +0900

    searchBar ui 개선

--- yeohaeng-gadam-fe/src/components/SearchBar.tsx
+++ yeohaeng-gadam-fe/src/components/SearchBar.tsx
@@ -120,68 +120,65 @@ export default function SearchBar() {
     }, []);
 
     return (
-        <div className="sticky top-0 z-[100] bg-white pt-1 pb-3 shadow">
+        <div className="sticky top-0 z-[100] bg-white pt-1 pb-3 shadow rounded-xl">
             <div className="flex flex-low px-10 mt-2">
-                <div className="basis-1/5">
-                    <input type="text"
-                        value={searchForm.location}
-                        onChange={e => setSearchForm({
-                            ...searchForm,
-                            location: e.target.value
-                        })}
-                        className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        placeholder="여행지" />
-                </div>
-                <div className="basis-1/5">
-                    <input type="date"
-                        onChange={e => setSearchForm({
-                            ...searchForm,
-                            start_date: e.target.value,
-                        })}
-                        value={searchForm.start_date}
-                        className="block w-full rounded-md border-0 text-center h-full px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                </div>
-                <div className="basis-1/5">
-                    <input type="date"
-                        value={searchForm.end_date}
-                        onChange={e => setSearchForm({
-                            ...searchForm,
-                            end_date: e.target.value
-                        })}
-                        className="block w-full rounded-md border-0 text-center h-full px-4  text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
+                <div className="w-3/5 flex items-center justify-between bg-gray-200 rounded-md">
+                    <div className="w-1/3">
+                        <input
+                            type="text"
+                            value={searchForm.location}
+                            onChange={e => setSearchForm({...searchForm, location: e.target.value})}
+                            className="block w-full rounded-md border-0 py-1.5 pl-2 pr-4 text-gray-900 bg-gray-200 placeholder:text-gray-400 focus:ring-1 focus:bg-white focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="여행지"
+                        />
+                    </div>
+                    <div className="w-1/3 h-full">
+                        <input
+                            type="date"
+                            onChange={e => setSearchForm({...searchForm, start_date: e.target.value})}
+                            value={searchForm.start_date}
+                            className="block w-full rounded-md border-0 text-center h-full px-2 text-gray-900 bg-gray-200 placeholder:text-gray-400 focus:ring-1 focus:bg-white focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    </div>
+                    <div className="w-1/3 h-full">
+                        <input
+                            type="date"
+                            value={searchForm.end_date}
+                            onChange={e => setSearchForm({...searchForm, end_date: e.target.value})}
+                            className="block w-full rounded-md border-0 text-center h-full px-2 text-gray-900 bg-gray-200 placeholder:text-gray-400 focus:ring-1 focus:bg-white focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    </div>
                 </div>
                 <div className="basis-1/5">
                     <button
                         onClick={handleSearch}
-                        className="nanumbarungothic ml-1 font-bold h-full text-center block w-full rounded-md border-0 py-1.5 px-auto text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-200 hover:text-gray-500 bg-gray-300">
+                        className="ml-1 nanumbarungothic font-bold h-full text-center block w-full rounded-md border-0 py-1.5 px-auto text-white hover:bg-blue-800  bg-blue-600">
                         방 찾기
                     </button>
                 </div>
                 <div className="basis-1/5 ml-2">
                     <button onClick={createRoomModal}
-                        className="nanumbarungothic font-bold h-full text-center block w-full rounded-md border-0 py-1.5 px-auto text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-200 hover:text-gray-500 bg-gray-300">
+                            className="nanumbarungothic font-bold h-full text-center block w-full rounded-md border-0 py-1.5 px-auto text-white hover:bg-blue-800  bg-blue-600">
                         방 만들기
                     </button>
                 </div>
             </div>
             <div className="ml-10 mt-2">
-                {tags.map(({ id, name }) => (
-                    <button key={id}
-                        className={`${activeTags.includes(name) ? "bg-blue-300" : "bg-blue-100"
-                            } rounded w-auto px-2 mx-2 mt-1 hover:bg-blue-300 nanumbarungothic h-8`}
-                        onClick={() => handleTagClick(name)}
-                    >
-                        {name}
-                    </button>
-                )
+                {tags.map(({id, name}) => (
+                        <button key={id}
+                                className={`${activeTags.includes(name) ? "bg-blue-600 text-white" : "bg-white ring-1 ring-gray-400"
+                                } text-sm rounded-2xl w-auto px-2 mx-2 mt-1 hover:bg-blue-100 nanumbarungothic h-8`}
+                                onClick={() => handleTagClick(name)}
+                        >
+                            {name}
+                        </button>
+                    )
                 )}
             </div>
             {isOpen && (
-                <RoomCreateModal onClose={closeModal} />
+                <RoomCreateModal onClose={closeModal}/>
             )}
-            
+
         </div>
     );
 }
--- yeohaeng-gadam-fe/src/pages/Home.tsx
+++ yeohaeng-gadam-fe/src/pages/Home.tsx
@@ -1,8 +1,9 @@
+import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import SearchBar from "../components/SearchBar.tsx";
 import RoomList from "../components/RoomList.tsx";
 import PopularLocation from "../components/PopularLocation.tsx";
-import {useEffect, useState} from "react";
+import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -24,7 +25,8 @@ const Home = () => {
         }, 3000);
 
         return () => clearInterval(intervalId);
-    }, []);
+    }, [images.length]);
+
     return (
         <main className="w-full h-screen top-0">
             <div className="h-[450px] w-full relative overflow-hidden">
@@ -38,14 +40,33 @@ const Home = () => {
                         alt={`Image ${index}`}
                     />
                 ))}
+                <div className="absolute top-0 left-0 w-full h-[450px] flex items-center justify-center">
+                    <div>
+                        <h3 className="text-white text-2xl md:text-5xl font-bold">여행 계획을 짤 땐</h3>
+                        <h3 className="text-white text-2xl md:text-5xl font-bold text-center">여행가담</h3>
+                    </div>
+
+                </div>
             </div>
-            <div className="h-auto">
+            <div className="h-auto px-4 sm:px-6 lg:px-8 py-6">
                 <SearchBar/>
-                <PopularLocation/>
-                <RoomList/>
+                <PopularLocation />
+                <RoomList />
             </div>
+            <ToastContainer
+                position="top-center"
+                autoClose={1500}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </main>
-    )
+    );
 }
 
-export default Home
+export default Home;
